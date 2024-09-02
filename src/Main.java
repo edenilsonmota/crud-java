62b7db7d708c787211203a8b5ff95f0e0d06bad5
Edenilson Mota <edenilsonmota.job@gmail.com>
2024-09-02 11:12:17 -0300

    Controller, DAO getAll

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import controller.PeopleController;
+import java.util.List;
+import model.People;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        PeopleController people = new PeopleController();
+
+        List<People> allPeople = people.getAll();
+
+        for(People person: allPeople){
+            System.out.println("ID: " + person.getId());
+            System.out.println("CPF: " + person.getCpf());
+            System.out.println("Name: " + person.getName());
+            System.out.println("Email: " + person.getEmail());
+            System.out.println("------");
+        }
+
+       // System.out.println(people.setPeople("22117357890", "Alice Giovana Catarina Brito", "alice_giovana_brito@crsilvadesign.com"));
     }
 }
--- src/model/People.java
+++ src/model/People.java
@@ -1,14 +1,27 @@
-package controller;
+package model;
 
 public class People {
+    private int id;
     private String cpf;
     private String name;
     private String email;
 
-    public People(String cpf, String name, String email){
+    public People(int id, String cpf, String name, String email){
+        this.id = id;
         this.cpf = cpf;
         this.name = name;
         this.email = email;
+    }
+
+    public People(){
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getCpf() {
